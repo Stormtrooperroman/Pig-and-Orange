0c705014797ca16f5dd29e3be20f24c23c4265f5
Stormtrooperroman <stormtrooperroman@gmail.com>
2018-12-09 12:54:46 +0700

    New JS and IMG

--- script.js
+++ script.js
@@ -1,57 +1,123 @@
 document.getElementsByTagName('body')[0].insertAdjacentHTML('beforeEnd', '<img src="orange.png" id="orange">');
-var pig = document.getElementById('orange')
+var orange = document.getElementById('orange');
 orange.style.position = 'fixed';
+orange.style.display = 'none';
+
 document.getElementsByTagName('body')[0].insertAdjacentHTML('beforeEnd', '<img src="pig.png" id="pig">');
-var pig = document.getElementById('pig')
+var pig = document.getElementById('pig');
 pig.style.position = 'fixed';
+pig.style.display = 'none';
+
 document.getElementsByTagName('body')[0].insertAdjacentHTML('beforeEnd', '<div id = "scoreObj"></div>');
 var scoreObj = document.getElementById('scoreObj');
-scoreObj.style.textAlign = "center";
-scoreObj.style.fontSize = 72 + "pt"
-var score = 0;
-var speedX =0;
-var speedY = 0;
-setScore(0);
+scoreObj.style.display = 'none';
 
+document.getElementsByTagName('body')[0].insertAdjacentHTML('beforeEnd', '<div id = "helloText">Нажмите клавишу Enter чтобы начать</div>');
+var helloText = document.getElementById('helloText');
+helloText.style.display = 'block';
+helloText.style.textAlign = "center";
+helloText.style.fontSize = 72 + "pt";
+scoreObj.style.fontSize = 72 + "pt";
+
+document.getElementsByTagName('body')[0].insertAdjacentHTML('beforeEnd', '<div id ="timerObj"></div>');
+var timerObj = document.getElementById('timerObj');
+timerObj.style.textAlign = "center";
+timerObj.style.fontSize = 72 + "pt";
+timerObj.style.display = "none";
+
+scoreObj.style.marginRight =50+ "px";
+
+var minY = 172;
+var timer =13;
+var intervalId;
+
+setScore(0);
 spawnOrange();
 
-var mouseListener = function(event)
-{
-	mouseMoveFunc(event);
-};
+
+var enterListener = function(event){startGame(event);}
+var mouseListener = function(event){mouseMoveFunc(event);}
+
 function mouseMoveFunc(event)
 {
-	pig.style.left =event.clientX - 64 + 'px';
-	pig.style.top = event.clientY -64 +'px';
+	pig.style.left = event.clientX - 64 + 'px';
+	pig.style.top = event.clientY -64 + 'px';
+
+	if (event.clientY < minY )
+	{
+		pig.style.top = minY - 64 + 'px';
+	} 
+
 	checkCollision();
-	moveOrange();
 }
+
 function spawnOrange()
 {
-	speedX= 10;
-	speedY=1;
-	orange.style.left =Math.random()*( window.innerWidth/2  -128) +'px';
+	orange.style.left =Math.random()*( window.innerWidth  -128) +'px';
 	orange.style.top = Math.random()*(window.innerHeight -128) +'px' ;
-}
-function moveOrange()
-{
-	orange.style.left = parseInt(orange.style.left + speedX) + 'px';
-	orange.style.top += speedY + 'px';
 }
 
 function checkCollision()
 {
-	console.log();
-	if ( Math.sqrt(Math.pow(pig.offsetLeft -orange.offsetLeft,2)+Math.pow(pig.offsetTop - orange.offsetTop,2)) <200)
+	while( Math.sqrt(Math.pow(pig.offsetLeft -orange.offsetLeft,2)+Math.pow(pig.offsetTop - orange.offsetTop,2)) <200)
 	{
 		spawnOrange();
+		pig.src = "pig2.png";
 		score+=5;
 		setScore(score);
 	}
 }
+
 function setScore(scoreToSet)
 {
 	scoreObj.innerHTML= "Очки " + scoreToSet;
 }
 
-document.addEventListener("mousemove",mouseListener);
+function startGame(event)
+{
+	if (event.keyCode ==13) 
+	{
+		score=0;
+		setScore(0);
+
+		helloText.style.display = 'none';
+		scoreObj.style.display = 'inline-block';
+		orange.style.display = 'block';
+		pig.style.display = 'block';
+		timerObj.style.display = 'inline-block';
+
+		document.removeEventListener('keydown', enterListener)
+		document.addEventListener("mousemove",mouseListener);
+		spawnOrange();
+
+		timer = 15;
+		setTimer(timer);
+		intervalId = setInterval(function(){cntdwn();},1000)
+	}
+}
+
+function setTimer(timeToSet){
+	console.log(timer);
+	timerObj.innerHTML = "Время: " + timeToSet;
+}
+
+function cntdwn()
+{
+	timer-=1
+	setTimer(timer);
+	if (timer==0)
+	{
+		clearInterval(intervalId);
+		document.removeEventListener('mousemove',mouseListener);
+		helloText.innerHTML = "Конец игрыб результат: " + score +". <br> Чтобы начать игру заново, нажмите Enter.";
+		helloText.style.display = 'block'
+		scoreObj.style.display = 'none';
+		orange.style.display = 'none';
+		pig.style.display = 'none';
+		timerObj.style.display = 'none';
+		document.addEventListener("keydown",enterListener);
+
+	}
+}
+
+document.addEventListener("keydown",enterListener);
